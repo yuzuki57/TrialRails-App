88635dcbb22de56f2a8b2cec150ffbfecf1ebe00
yuzuki57 <taburisu47@gmail.com>
2019-08-25 21:28:18 +0900

    マイページの作成（簡易）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       redirect_to mypage_path
     else
       render 'home/index'
