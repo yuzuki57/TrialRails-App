7c6ab8f2af41eb20e429c8c07de1b82170851ba3
yuzuki57 <taburisu47@gmail.com>
2019-08-25 17:43:58 +0900

    ハッシュタグ機能の追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,8 @@ class BoardsController < ApplicationController
     before_action :set_target_board, only: %i[show edit uodate destroy]
 
     def index
-        @boards = Board.page(params[:page])
+        @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
+        @boards = @boards.page(params[:page])
     end
 
     def new
@@ -27,12 +28,17 @@ class BoardsController < ApplicationController
         
     end
     def update
-        @board.update(boards_params)
-
-        redirect_to @board
+        if @board.update(boards_params)
+            redirect_to @board
+        else
+            redirect_to :back, flash: {
+              board: @board,
+              error_messages: @board.errors.full_messages
+            }
+        end
     end
     def destroy
-        @board.delete
+        @board.destroy
 
         redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました"}
     end
@@ -40,7 +46,7 @@ class BoardsController < ApplicationController
     private
 
     def boards_params
-        params.require(:board).permit(:name, :title, :body)
+        params.require(:board).permit(:name, :title, :body, tag_ids: [])
     end
     def set_target_board
         @board = Board.find(params[:id])
--- app/models/board.rb
+++ app/models/board.rb
@@ -11,8 +11,8 @@
 #
 
 class Board < ApplicationRecord
-    has_many :comments
-    has_many :Board_tag_relations
+    has_many :comments, dependent: :delete_all
+    has_many :Board_tag_relations, dependent: :delete_all
     has_many :tags, through: :Board_tag_relations
 
     validates :name, presence: true, length: { maximum: 10 }
