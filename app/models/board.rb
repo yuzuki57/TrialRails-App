5349ef692d2ca3f82c11e91f7325cb7968f6ba7e
yuzuki57 <taburisu47@gmail.com>
2019-08-25 15:15:59 +0900

    モデルのアソシエーション追加、アソシエーション用のGem追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  body       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Board < ApplicationRecord
+    has_many :comments
+
     validates :name, presence: true, length: { maximum: 10 }
     validates :title, presence: true, length: { maximum: 30 }
     validates :body, presence: true, length: { maximum: 1000 }
