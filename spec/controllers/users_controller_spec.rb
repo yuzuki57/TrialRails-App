4f13a21bd42c4825b0b8fbfdaaa46e7bfb4dbbc9
yuzuki57 <taburisu47@gmail.com>
2019-08-26 17:35:34 +0900

    コントローラーのテストコード追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,6 +17,11 @@ RSpec.describe UsersController, type: :controller do
         end
     end
     describe 'POST #create' do
+        before do
+            @referer = 'http://localhost'
+            @request.env['HTTP_REFERER'] = @referer
+        end
+
         context '正しいユーザー情報が渡ってきた場合' do
             let(:params) do
                 { user: {
@@ -33,6 +38,30 @@ RSpec.describe UsersController, type: :controller do
 
             it 'マイページにリダイレクトされること' do
                 expect(post :create, params: params).to redirect_to(mypage_path)
+            end
+        end
+
+        context 'パラメータに正しいユーザー名、確認パスワードが含まれていない場合' do
+            before do
+                post(:create, params: {
+                    user: {
+                        name: 'ユーザー１',
+                        password: 'password',
+                        password_confirmation: 'invalid_password'
+                    }
+                })
+            end
+
+            it 'リファラーにリダイレクトされること' do
+                expect(response).to redirect_to(@referer)
+            end
+
+            it 'ユーザー名にエラーメッセージが含まれていること' do
+                expect(flash[:error_messages]).to include 'ユーザー名は小文字英数字で入力してください'
+            end
+
+            it 'パスワード確認のエラーメッセージが含まれていること' do
+                expect(flash[:error_messages]).to include 'パスワード（確認）とパスワードの入力が一致しません'
             end
         end
     end
