ca67ca40662df333d9d2cf7a2f856bdf3cd55db6
yuzuki57 <taburisu47@gmail.com>
2019-08-26 17:16:48 +0900

    コントローラーテストコードの追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,15 +5,35 @@ RSpec.describe UsersController, type: :controller do
         before { get :new }
 
         it 'レスポンスコードが200であること' do
-            expect(response).to have_http_status(:ok)
+          expect(response).to have_http_status(:ok)
         end
 
         it 'newテンプレートをレンダリングすること' do
-            expect(response).to render_template :new
+          expect(response).to render_template :new
         end
 
         it '新しいuserオブジェクトがビューに渡されること' do
-            expect(assigns(:user)).to be_a_new User
+          expect(assigns(:user)).to be_a_new User
+        end
+    end
+    describe 'POST #create' do
+        context '正しいユーザー情報が渡ってきた場合' do
+            let(:params) do
+                { user: {
+                    name: 'user',
+                    password: 'password',
+                    password_confirmation: 'password'
+                  }
+                }
+            end
+
+            it 'ユーザーが一人増えていること' do
+                expect { post :create, params: params }.to change(User, :count).by(1)
+            end
+
+            it 'マイページにリダイレクトされること' do
+                expect(post :create, params: params).to redirect_to(mypage_path)
+            end
         end
     end
 end
